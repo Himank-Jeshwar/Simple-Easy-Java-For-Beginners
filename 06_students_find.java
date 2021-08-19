59a89ad913f468b2bc80023251b3e2363ab845b5
Himank-Jeshwar <technicalhimank@gmail.com>
2021-08-19 14:31:21 +0530

    Added New File on 19/08/21

---
+++ 06_students_find.java
@@ -0,0 +1,14 @@
+class Students_06 
+{
+    public static void main(String[] args) 
+    {
+        // declare
+        int totstud=800;
+        int girls=28*totstud/100;
+        // calculating 
+        int boys = totstud-girls;
+        //output
+        System.out.println("Number of Girls = "+girls);
+        System.out.print("Number of Boys = "+boys);    
+    }
+}
